1bcd8d617075bcf28e11addfdf8c399e615d29a4
mathieu2em <math.per@hotmail.com>
2019-04-28 00:45:14 -0400

    going to bed will continue from other computer. gonna setup the recyclerView

--- app/src/main/java/com/example/mainactivity/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/mainactivity/RecyclerViewAdapter.java
@@ -1,16 +1,62 @@
 package com.example.mainactivity;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
-public class RecyclerViewAdapter {
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
 
     private static final String TAG = "RecyclerViewAdapter";
+
+    private ArrayList<String> mImgNames = new ArrayList<>();
+    private ArrayList<String> mImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(Context mContext, ArrayList<String> mImgNames, ArrayList<String> mImages) {
+        this.mImgNames = mImgNames;
+        this.mImages = mImages;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        //the layout inflater is to fill the Layout with something.
+        /* LayoutInflater is used to create a new View (or Layout) object from one of your xml layouts.*/
+        View view = LayoutInflater.from( parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+
+        Log.d(TAG, "onBindViewHolder: called");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(mImages.get(position))
+                .into(viewHolder.image);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
